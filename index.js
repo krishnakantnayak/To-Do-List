aae0a4c2abccbe039fdeb68ca46ef67a19bf615c
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-18 19:33:20 +0530

    mongoose.set('strictQuery', true)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=8080;
 const db=require('./config/mongoose');
-mongoose.set('strictQuery', true)
+
 app.use(express.urlencoded());
 
 //below line uses the routes to different controlers for the application
